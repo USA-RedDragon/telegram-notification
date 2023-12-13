6686f5e94e8fc664812a08df236a5d96b4fe9a81
Jacob McSwain <jacob.a.mcswain@gmail.com>
2023-12-13 13:25:43 -0600

    escape # in issue number

--- src/main.ts
+++ src/main.ts
@@ -48,7 +48,7 @@ export async function run(): Promise<void> {
         const issueUser = escapeMarkdown(github.context.payload.issue?.user?.login! as string);
         const issueUserLink = escapeMarkdown(github.context.payload.issue?.user?.html_url! as string);
 
-        let issueMarkdown = `[#${issueNumber}](${issueLink}) by [${issueUser}](${issueUserLink}) in [${repoName}](${repoLink}) ${action}\n\n`;
+        let issueMarkdown = `[\\\\\\#${issueNumber}](${issueLink}) by [${issueUser}](${issueUserLink}) in [${repoName}](${repoLink}) ${action}\n\n`;
         issueMarkdown += `Title: **${issueTitle}**\n\n`;
         // For each line in the body, add a > to make it a quote
         issueMarkdown += issueBody.split('\n').map((line) => `> ${line}`).join('\n');
