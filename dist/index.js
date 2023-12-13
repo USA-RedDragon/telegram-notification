5c9e380b10cc3af7bd3290a51c3e42485ee2b93f
Jacob McSwain <jacob.a.mcswain@gmail.com>
2023-12-13 14:01:27 -0600

    Add discussion message

--- dist/index.js
+++ dist/index.js
@@ -28970,18 +28970,18 @@ async function run() {
     try {
         const botToken = core.getInput('bot-token');
         const chatID = core.getInput('chat-id');
+        const repoLink = escapeMarkdown(github.context.payload.repository?.html_url);
+        const repoName = escapeMarkdown(github.context.payload.repository?.name);
+        const action = escapeMarkdown(github.context.payload.action);
         switch (github.context.eventName) {
             case 'issues':
                 const issueNumber = escapeMarkdown(github.context.payload.issue?.number.toString());
                 const issueLink = escapeMarkdown(github.context.payload.issue?.html_url);
-                const repoLink = escapeMarkdown(github.context.payload.repository?.html_url);
-                const repoName = escapeMarkdown(github.context.payload.repository?.name);
-                const action = escapeMarkdown(github.context.payload.action);
                 const issueTitle = escapeMarkdown(github.context.payload.issue?.title);
                 const issueBody = escapeMarkdown(github.context.payload.issue?.body);
                 const issueUser = escapeMarkdown(github.context.payload.issue?.user?.login);
                 const issueUserLink = escapeMarkdown(github.context.payload.issue?.user?.html_url);
-                let issueMarkdown = `[\\#${issueNumber}](${issueLink}) by [${issueUser}](${issueUserLink}) in [${repoName}](${repoLink}) ${action}\n\n`;
+                let issueMarkdown = `Issue [\\#${issueNumber}](${issueLink}) by [${issueUser}](${issueUserLink}) in [${repoName}](${repoLink}) ${action}\n\n`;
                 issueMarkdown += `> ${issueTitle}\n\n`;
                 // For each line in the body, add a > to make it a quote
                 issueMarkdown += issueBody.split('\n').map((line) => `> ${line}`).join('\n');
@@ -28997,7 +28997,6 @@ async function run() {
                     },
                     body: JSON.stringify(issuePayload),
                 };
-                console.log(JSON.stringify(issuePayload));
                 fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, issueOptions).then((response) => {
                     if (!response.ok) {
                         core.setFailed(`HTTP error ${response.status}`);
@@ -29008,6 +29007,36 @@ async function run() {
                 });
                 break;
             case 'discussion':
+                const discussionNumber = escapeMarkdown(github.context.payload.discussion?.number.toString());
+                const discussionLink = escapeMarkdown(github.context.payload.discussion?.html_url);
+                const discussionTitle = escapeMarkdown(github.context.payload.discussion?.title);
+                const discussionBody = escapeMarkdown(github.context.payload.discussion?.body);
+                const discussionUser = escapeMarkdown(github.context.payload.discussion?.user?.login);
+                const discussionUserLink = escapeMarkdown(github.context.payload.discussion?.user?.html_url);
+                let discussionMarkdown = `Discussion [\\#${discussionNumber}](${discussionLink}) by [${discussionUser}](${discussionUserLink}) in [${repoName}](${repoLink}) ${action}\n\n`;
+                discussionMarkdown += `> ${discussionTitle}\n\n`;
+                // For each line in the body, add a > to make it a quote
+                discussionMarkdown += discussionBody.split('\n').map((line) => `> ${line}`).join('\n');
+                const discussionPayload = {
+                    chat_id: chatID,
+                    text: discussionMarkdown,
+                    parse_mode: 'MarkdownV2',
+                };
+                const discussionOptions = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(discussionPayload),
+                };
+                fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, discussionOptions).then((response) => {
+                    if (!response.ok) {
+                        core.setFailed(`HTTP error ${response.status}`);
+                    }
+                }).catch((e) => {
+                    console.error(e);
+                    core.setFailed(e);
+                });
                 break;
             default:
                 core.setFailed('Event not supported');
--- src/main.ts
+++ src/main.ts
@@ -36,19 +36,20 @@ export async function run(): Promise<void> {
     const botToken = core.getInput('bot-token');
     const chatID = core.getInput('chat-id');
 
+    const repoLink = escapeMarkdown(github.context.payload.repository?.html_url!);
+    const repoName = escapeMarkdown(github.context.payload.repository?.name!);
+    const action = escapeMarkdown(github.context.payload.action!);
+
     switch (github.context.eventName) {
       case 'issues':
         const issueNumber = escapeMarkdown(github.context.payload.issue?.number!.toString() as string);
         const issueLink = escapeMarkdown(github.context.payload.issue?.html_url!);
-        const repoLink = escapeMarkdown(github.context.payload.repository?.html_url!);
-        const repoName = escapeMarkdown(github.context.payload.repository?.name!);
-        const action = escapeMarkdown(github.context.payload.action!);
         const issueTitle = escapeMarkdown(github.context.payload.issue?.title!);
         const issueBody = escapeMarkdown(github.context.payload.issue?.body!);
         const issueUser = escapeMarkdown(github.context.payload.issue?.user?.login! as string);
         const issueUserLink = escapeMarkdown(github.context.payload.issue?.user?.html_url! as string);
 
-        let issueMarkdown = `[\\#${issueNumber}](${issueLink}) by [${issueUser}](${issueUserLink}) in [${repoName}](${repoLink}) ${action}\n\n`;
+        let issueMarkdown = `Issue [\\#${issueNumber}](${issueLink}) by [${issueUser}](${issueUserLink}) in [${repoName}](${repoLink}) ${action}\n\n`;
         issueMarkdown += `> ${issueTitle}\n\n`;
         // For each line in the body, add a > to make it a quote
         issueMarkdown += issueBody.split('\n').map((line) => `> ${line}`).join('\n');
@@ -67,8 +68,6 @@ export async function run(): Promise<void> {
           body: JSON.stringify(issuePayload),
         };
 
-        console.log(JSON.stringify(issuePayload))
-
         fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, issueOptions).then((response: Response) => {
           if (!response.ok) {
             core.setFailed(`HTTP error ${response.status}`);
@@ -80,6 +79,41 @@ export async function run(): Promise<void> {
 
         break;
       case 'discussion':
+        const discussionNumber = escapeMarkdown(github.context.payload.discussion?.number!.toString() as string);
+        const discussionLink = escapeMarkdown(github.context.payload.discussion?.html_url!);
+        const discussionTitle = escapeMarkdown(github.context.payload.discussion?.title!);
+        const discussionBody = escapeMarkdown(github.context.payload.discussion?.body!);
+        const discussionUser = escapeMarkdown(github.context.payload.discussion?.user?.login! as string);
+        const discussionUserLink = escapeMarkdown(github.context.payload.discussion?.user?.html_url! as string);
+
+        let discussionMarkdown = `Discussion [\\#${discussionNumber}](${discussionLink}) by [${discussionUser}](${discussionUserLink}) in [${repoName}](${repoLink}) ${action}\n\n`;
+        discussionMarkdown += `> ${discussionTitle}\n\n`;
+        // For each line in the body, add a > to make it a quote
+        discussionMarkdown += discussionBody.split('\n').map((line) => `> ${line}`).join('\n');
+
+        const discussionPayload = {
+          chat_id: chatID,
+          text: discussionMarkdown,
+          parse_mode: 'MarkdownV2',
+        };
+
+        const discussionOptions = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(discussionPayload),
+        };
+
+        fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, discussionOptions).then((response: Response) => {
+          if (!response.ok) {
+            core.setFailed(`HTTP error ${response.status}`);
+          }
+        }).catch((e: any) => {
+          console.error(e);
+          core.setFailed(e);
+        });
+
         break;
       default:
         core.setFailed('Event not supported');
